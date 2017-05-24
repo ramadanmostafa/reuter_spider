39242604c0ee1ae213ea9d527d402590f78da64f
Ramadan K.Mostafa <ramadankhalifamostafa@gmail.com>
2017-05-24 13:32:48 +0200

    create database tables

--- reuters/reuters/settings.py
+++ reuters/reuters/settings.py
@@ -91,3 +91,6 @@ ITEM_PIPELINES = {
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+# DATABASE settings
+DATABASE_NAME = "reuters_db.sqlite"
